dd015ef3ce37a523d0976486e7850ebef13fb2bd
daniel watson <ozzloy@each.do>
2025-01-15 20:52:29 -0800

    make start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import "./App.css";
 
 function App() {
+  const handleLogin = () => {
+    alert("login clicked!");
+  };
   return (
-    <main>
+    <>
       <header>
-        <h1>hello, taxees!</h1>
+        <nav>
+          <h1>tax front</h1>
+          <button onClick={handleLogin}>login</button>
+        </nav>
       </header>
-      <article>
-        <p>fill usa tax form 1040 for entertainment purposes.</p>
-      </article>
-      <footer>
-        <small>built with love and kindness</small>
-      </footer>
-    </main>
+      <main>
+        <article>
+          <p>fill usa tax form 1040 for entertainment purposes.</p>
+        </article>
+        <footer>
+          <small>built with love and kindness</small>
+        </footer>
+      </main>
+    </>
   );
 }
 
