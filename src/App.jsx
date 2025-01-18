3bb8a653c197e97a40837d0acf952b9f7abd83b2
daniel watson <ozzloy@each.do>
2025-01-18 09:17:43 -0800

    make theme select cards

--- src/App.jsx
+++ src/App.jsx
@@ -38,12 +38,45 @@ const App = () => {
       <button onClick={handleFetchThemes}>fetch themes!</button>
       {themes &&
         themes.map(([themeId, themeData]) => (
-          <button
+          <section
             key={themeId}
-            onClick={() => handleThemeChange(themeId)}
+            style={{
+              padding: "1em",
+              margin: "1em 0",
+              border: "1px solid var(--foreground-color)",
+              borderRadius: "8px",
+              backgroundColor: themeData.background_color,
+              color: themeData.foreground_color,
+              borderColor: themeData.foreground_color,
+            }}
           >
-            {themeData.name}
-          </button>
+            <h2>{themeData.name}</h2>
+            <p style={{ margin: "1rem 0" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div style={{ marginBottom: "1em" }}>
+              <p>
+                created:{" "}
+                {new Date(themeData.created).toLocaleString()}
+              </p>
+              <p>
+                updated:{" "}
+                {new Date(themeData.updated).toLocaleString()}
+              </p>
+            </div>
+            <button
+              onClick={() => handleThemeChange(themeId)}
+              style={{
+                backgroundColor: themeData.background_color,
+                color: themeData.foreground_color,
+                borderColor: themeData.foreground_color,
+              }}
+            >
+              apply {themeData.name} theme
+            </button>
+          </section>
         ))}
       <main>
         <article>
