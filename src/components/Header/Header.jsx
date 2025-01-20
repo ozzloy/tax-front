264bd8750b48a09ab449b3571085256930b16344
daniel watson <ozzloy@each.do>
2025-01-20 09:08:49 -0800

    navigate home after logout, also reset to default theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,47 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import { setCurrentModal } from "../../store/uiSlice";
+import {
+  setCurrentModal,
+  setActiveThemeId,
+} from "../../store/uiSlice";
+import { fetchThemes } from "../../store/themeSlice";
 import Modal from "../Modal";
 import "./Header.css";
 import Login from "../Login";
 import { getKing, logout } from "../../store/authSlice";
 import Signup from "../Signup";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { currentModal } = useSelector((state) => state.ui);
   const { king } = useSelector((state) => state.auth);
+  const themes = useSelector((state) => state.theme.theme);
 
   useEffect(() => {
     dispatch(getKing());
   }, [dispatch]);
 
-  const handleAuthOptions = () => {
+  const findLowestSystemThemeId = (themes) => {
+    const systemThemes = Object.values(themes)
+      .filter((theme) => theme.king_id === null)
+      .sort((a, b) => a.id - b.id);
+    return systemThemes[0]?.id;
+  };
+
+  const handleAuthOptions = async () => {
     if (king) {
-      dispatch(logout());
+      const result = await dispatch(logout());
+      if (logout.fulfilled.match(result)) {
+        await dispatch(fetchThemes());
+        const lowestSystemThemeId = findLowestSystemThemeId(themes);
+        if (lowestSystemThemeId) {
+          dispatch(setActiveThemeId(lowestSystemThemeId));
+        }
+        navigate("/home");
+      }
     } else {
       dispatch(setCurrentModal("auth"));
     }
