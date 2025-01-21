0c93056c2bb964063de7ba89b51722a9611ab387
daniel watson <ozzloy@each.do>
2025-01-21 05:12:17 -0800

    add nice explainer text to home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,27 @@
+import { useSelector } from "react-redux";
 import "./Home.css";
+import { selectCurrentKing } from "../../store/kingSlice";
 
 const Home = () => {
-  return <h2 className="home">home</h2>;
+  const king = useSelector(selectCurrentKing);
+  return (
+    <>
+      <h2 className="home">home</h2>
+      <h3>{(king ? "now that" : "once") + " you are logged in,"}</h3>
+      <ol style={{ listStyleType: "decimal", padding: "2em" }}>
+        <li>
+          enter some info about the people filing taxes under the
+          &quot;human&quot; section.
+        </li>
+        <li>
+          then enter address info in the &quot;address&quot; section
+        </li>
+        <li>
+          finally, save your form 1040 info in the &quot;1040&quot;
+          section.
+        </li>
+      </ol>
+    </>
+  );
 };
 export default Home;
